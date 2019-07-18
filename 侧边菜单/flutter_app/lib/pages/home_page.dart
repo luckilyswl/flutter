ce1242e6340b6b6b5f3f33fb22540bb7a4205314
luckilyswl <792411500@qq.com>
2019-07-18 11:49:44 +0800

    侧边菜单  的布局

---
+++ 侧边菜单/flutter_app/lib/pages/home_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_app/pages/other_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text("CYC"),
+        backgroundColor: Colors.redAccent,
+      ),
+      //侧边菜单按钮
+      drawer: new Drawer(
+        //填充内容
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('CYC'),
+              //用户名
+              accountEmail: new Text('792411500@qq.com'),
+              //用户邮箱
+              currentAccountPicture: new GestureDetector(
+                //用户头像
+                onTap: () => print('current user'),
+                child: new CircleAvatar( //圆形图标控件
+                  backgroundImage: new NetworkImage(
+                      'https://upload.jianshu.io/users/upload_avatars/7700793/dbcf94ba-9e63-4fcf-aa77-361644dd5a87?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                ),
+              ),
+              otherAccountsPictures: <Widget>[
+                //粉丝头像
+                new GestureDetector(
+                  //手势探测器，可以识别各种手势，这里只用到了onTap
+                  onTap: () => print('other user'), //暂且先打印一下信息吧，以后再添加跳转页面的逻辑
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage(
+                        'https://upload.jianshu.io/users/upload_avatars/10878817/240ab127-e41b-496b-80d6-fc6c0c99f291?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                  ),
+                ),
+                new GestureDetector(
+                  onTap: () => print('other user'),
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage(
+                        'https://upload.jianshu.io/users/upload_avatars/8346438/e3e45f12-b3c2-45a1-95ac-a608fa3b8960?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                  ),
+                ),
+              ],
+              decoration: new BoxDecoration(
+                //用一个BoxDecoration装饰器提供背景图片
+                image: new DecorationImage(
+                  fit: BoxFit.fill,
+                  // image: new NetworkImage('https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg')
+                  //可以试试图片调取自本地。调用本地资源，需要到pubspec.yaml中配置文件路径
+                  image: new NetworkImage('https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3398958699,4002206613&fm=27&gp=0.jpg'),
+                ),
+              ),
+            ),
+            new ListTile(   //第一个功能项
+                title: new Text('First Page'),
+                trailing: new Icon(Icons.arrow_upward),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) => new OtherPage("First Page")
+                      )
+                  );
+                }
+            ),
+            new ListTile(   //第二个功能项
+                title: new Text('Second Page'),
+                trailing: new Icon(Icons.arrow_right),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) => new OtherPage("Second Page")
+                      ),
+                  );
+                }
+            ),
+            //
+            /*new ListTile(   //第二个功能项
+                title: new Text('Second Page'),
+                trailing: new Icon(Icons.arrow_right),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pushNamed('/a');
+                }
+            ),*/
+            new Divider(),    //分割线控件
+            new ListTile(   //退出按钮
+              title: new Text('关闭'),
+              trailing: new Icon(Icons.cancel),
+              onTap: () => Navigator.of(context).pop(),   //点击后收起侧边栏
+            ),
+          ],
+        ),
+      ),
+      body: new Center(
+        //中央内容部分body
+        child: new Text(
+          "HomePage",
+          style: new TextStyle(fontSize: 35),
+        ),
+      ),
+    );
+  }
+}
